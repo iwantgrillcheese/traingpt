a50351b64186ed536377fdcbf5b413da152bcbdd
iwantgrillcheese <cameron.mcdiarmid@gmail.com>
2025-05-28 17:46:48 +0000

    seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.traingpt.co',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  sitemapSize: 5000,
+};
