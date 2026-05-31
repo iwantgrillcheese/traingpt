6c96add66c4b55f24bb0dd0d41f79c904d4fb135
iwantgrillcheese <cameron.mcdiarmid@gmail.com>
2026-05-31 14:03:27 -0700

    Remove corrupted native App entrypoint

--- mobile/src/App.tsx
+++
@@ -1,5 +0,0 @@
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AuthProvider, useAuth } from './auth
