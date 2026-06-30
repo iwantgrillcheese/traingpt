6606b99fde5d61c5874aacd8258cada169a7abce
iwantgrillcheese <cameron.mcdiarmid@gmail.com>
2026-06-30 10:19:24 -0700

    Install plan generation fetch guard

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,11 +5,13 @@ import Layout from './components/Layout';
 import PostHogIdentityBridge from './components/PostHogIdentityBridge';
 import MagicUiObserverOverlay from '@/components/MagicUiObserverOverlay';
 import PlanReadyReviewOverlay from '@/components/PlanReadyReviewOverlay';
+import PlanGenerationFetchGuard from './components/PlanGenerationFetchGuard';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
       <PostHogIdentityBridge />
+      <PlanGenerationFetchGuard />
       <Layout>{children}</Layout>
       <MagicUiObserverOverlay />
       <PlanReadyReviewOverlay />
